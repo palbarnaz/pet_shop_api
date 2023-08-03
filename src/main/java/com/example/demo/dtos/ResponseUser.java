b1a6793bd05648ed07eea5a6491027566a525179
Paola Albarnaz <paola.silva.nh1@gmail.com>
2023-08-03 16:14:41 -0300

    refactor: melhora dto adicionando lista de animais

--- src/main/java/com/example/demo/dtos/ResponseUser.java
+++ src/main/java/com/example/demo/dtos/ResponseUser.java
@@ -1,8 +1,10 @@
 package com.example.demo.dtos;
 
 import com.example.demo.enums.Profile;
+import com.example.demo.models.Animal;
 import com.example.demo.models.User;
 
+import java.util.List;
 import java.util.UUID;
 
 public record ResponseUser(
@@ -10,12 +12,14 @@ public record ResponseUser(
         String name,
         String email,
         String phone,
-        Profile profile
+        Profile profile,
+
+        List<Animal> animal
 
 
 ) {
 
     public ResponseUser(User user) {
-        this(user.getId(), user.getName(), user.getEmail(), user.getPhone(), user.getProfile());
+        this(user.getId(), user.getName(), user.getEmail(), user.getPhone(), user.getProfile(), user.getAnimals());
     }
 }
